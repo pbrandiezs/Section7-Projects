f6b37e24a9464bc3d587900b29c7bc8225dd744e
Perry Brandiezs <pbrandiezs@gmail.com>
2025-06-02 18:19:05 -0700

    Initial commit

---
+++ src/HouseDemo.java
@@ -0,0 +1,24 @@
+public class HouseDemo {
+    public static void main(String[] args) {
+        House myHouse = new House();
+        House yourHouse = new House();
+
+        myHouse.setNumStories(2);
+        myHouse.setNumWindows(6);
+        myHouse.setColor("red");
+
+        yourHouse.setNumStories(3);
+        yourHouse.setNumWindows(10);
+        yourHouse.setColor("Blue");
+
+        System.out.println("My house is " + myHouse.getColor() +
+                " and has " + myHouse.getNumStories() + " stories and " +
+                myHouse.getNumWindows() + " windows.");
+
+        System.out.println();
+
+        System.out.println("Your house is " + yourHouse.getColor() +
+                " and has " + yourHouse.getNumStories() + " stories and " +
+                yourHouse.getNumWindows() + " windows.");
+    }
+}
